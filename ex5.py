2d9fdc7f1e57855922c278b7c28f3cf5cd9b079c
Chanrattnak Mong <82812258+rattnak@users.noreply.github.com>
2022-10-23 17:04:09 +0700

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,4 +1,7 @@
-rev = ""
-word = input("Enter a statement you want to reverse: ")
-rev += word[::-1]
-print(f"Your reversed word is {rev}")
+def backward_string_by_word(w):
+    rev = ""
+    rev += word[::-1]
+    return rev
+if __name__ == '__main__':
+    word = input("Enter a statement you want to reverse: ")
+    print(f"Your reversed word is {backward_string_by_word(word)}")
