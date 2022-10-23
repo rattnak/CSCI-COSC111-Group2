62d2d2ba6928b145e4f2871dda1c92aa7a851fea
davidheang <116336736+davidheang@users.noreply.github.com>
2022-10-23 22:02:04 +0700

    Update exercise-2.py

--- exercise-2.py
+++ exercise-2.py
@@ -2,6 +2,7 @@ def index_power(list,exponent):
     list1 = []
     if exponent == 0:
         return 1
+    
     elif len(list) > exponent:
         for i in list:
             expo = i ** exponent
@@ -10,12 +11,15 @@ def index_power(list,exponent):
     
     else:
         return -1
+list1 = ([1,2,3,4] ,2)
+list2 = ([1,5,100], 0)
+list3 = ([0,1,3], 5)
 
 if __name__ == '__main__':
 
     a = index_power([1,2,3,4], 2)
     b = index_power([1,5,100], 0 )
     c = index_power([0,1,3], 5)
-    print(a)
-    print(b)
-    print(c)
+    print(f'for{list1}, the return value is{a}.')
+    print(f'for{list2}, the return value is{b}.')
+    print(f'for{list3}, the return value is{c}.')
