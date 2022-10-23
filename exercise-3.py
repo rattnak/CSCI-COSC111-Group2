1fd2b74d9a19b18104ec515e81e43e37e4be3b85
Chanrattnak Mong <82812258+rattnak@users.noreply.github.com>
2022-10-23 22:29:20 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -5,7 +5,8 @@ def remove(list, S:int):
         cut = list.index(S)
         result = list[:cut + 1]
         return result
-final_result1 = remove([1, 2, 3, 4, 5], 3)
-final_result2 = remove([1, 1, 2, 2, 3, 3], 2)
-print(final_result1)
-print(final_result2)
+if __name__ == '__main__':
+    final_result1 = remove([1, 2, 3, 4, 5], 3)
+    final_result2 = remove([1, 1, 2, 2, 3, 3], 2)
+    print(final_result1)
+    print(final_result2)
