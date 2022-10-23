f7921d39a4f9eda37d12cd8380844a282ed60ad7
Chanrattnak Mong <82812258+rattnak@users.noreply.github.com>
2022-10-23 22:27:03 +0700

    Add files via upload

---
+++ exercise-3.py
@@ -0,0 +1,11 @@
+def remove(list, S:int):
+    if S not in list:
+        return list
+    else:
+        cut = list.index(S)
+        result = list[:cut + 1]
+        return result
+final_result1 = remove([1, 2, 3, 4, 5], 3)
+final_result2 = remove([1, 1, 2, 2, 3, 3], 2)
+print(final_result1)
+print(final_result2)
