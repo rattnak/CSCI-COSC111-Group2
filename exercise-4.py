94258123dde9b7620060dd6760eb9f61cef2847e
Sovatharothh <116065704+Sovatharothh@users.noreply.github.com>
2022-10-23 23:02:39 +0700

    Update exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,9 +1,18 @@
+
 def chunking_by(items: list, chunk_size: int):
     for i in range(len(items)):
         if i % chunk_size == 0:
             yield items[i: i + chunk_size]
 
+list1 = ([5, 4, 7, 3, 4, 5, 4], 3)
+list2 = ([3, 4, 5], 1)
+
 if __name__ == '__main__':
-      print(list(chunking_by([5, 4, 7, 3, 4, 5, 4], 3)))
-      
+
+    a = chunking_by([5, 4, 7, 3, 4, 5, 4], 3)
+    b = chunking_by([3, 4, 5], 1)
+    
+    print(f'for{list1}, the return value is {a}.')
+    print(f'for{list2}, the return value is {b}.')
+
 
